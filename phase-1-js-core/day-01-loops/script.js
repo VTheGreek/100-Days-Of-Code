342e1ee6ff494cbcb3219736870cae6954ac21f4
VTheGreek <vasilis.proefth@gmail.com>
2026-01-19 23:48:37 +0200

    Finished the 1st day of the challenge

--- phase-1-js-core/day-01-loops/script.js
+++ phase-1-js-core/day-01-loops/script.js
@@ -4,9 +4,47 @@ Calculate the sum of all numbers from 1 to n.
 Use a loop.
 */
 
-let num = 10;
-
-for (let n = 1; n <=num; n++) {
-    num = num + n;
+let n = Number(prompt("Give a number."));
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+    sum = sum + i;
+    console.log("i:", i, "sum:", sum);
 }
-console.log("n:", n, "num:", num);
+
+
+/*
+You receive an array of numbers.
+Find the largest number.
+No Math.max.
+Use a loop.
+*/
+
+let numArray = [13, 50, 1, 2, 77];
+let max = numArray[0];
+
+for (let i = 1; i < numArray.length; i++) {
+    if(numArray[i] > max) {
+        max = numArray[i];
+    }
+}
+console.log(max);
+
+
+/*
+You receive a string.
+Count how many times each character appears.
+The count is case-sensitive.
+Use a loop.
+*/
+
+
+let myString = "Hello World"
+let charCount = {};
+for (let char of myString) {
+    if (charCount[char]) {
+        charCount[char] += 1;
+    } else {
+        charCount[char] = 1;
+    }
+} 
+console.log(charCount);
