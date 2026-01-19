168adbe0f9aa2e21dd7189ff4c6208242fa7227d
VTheGreek <vasilis.proefth@gmail.com>
2026-01-19 17:38:26 +0200

    Added the phase 1 folder. Also HTML and JS

---
+++ phase-1-js-core/day-01-loops/script.js
@@ -0,0 +1,12 @@
+/*
+You receive a number n.
+Calculate the sum of all numbers from 1 to n.
+Use a loop.
+*/
+
+let num = 10;
+
+for (let n = 1; n <=num; n++) {
+    num = num + n;
+}
+console.log("n:", n, "num:", num);
